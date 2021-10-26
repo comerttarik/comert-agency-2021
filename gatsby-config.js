94be7d0d5c82bda08d0b0b7b686a423060af9264
Ahmet Tarik Comert <ahmettarik.comert@student.ap.be>
2021-10-26 13:25:40 +0200

    Gatbsy Plugins finished

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,4 @@
+/*
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -36,3 +37,17 @@ module.exports = {
     // `gatsby-plugin-offline`,
   ],
 }
+*/
+
+module.exports = {
+  siteMetadata: {
+    title: "Artist Agency",
+    description: "Artist Agency was founded in 1977 by founder, John Doe. AA continues to be at the forefront of art by establishing the careers of our talents on a holistic level -- and setting trends within the industry.",
+    author: "@gatsbyjs",
+    siteUrl: "https://gatsbystarterdefaultsource.gatsbyjs.io/",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -32,14 +32,41 @@ const IndexPage = () => (
 export default IndexPage
 */
 
+/*
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <main>
       <Layout pageTitle="Welcome to Inghelbrecht Agency!">
       <p>Lorem ipsum</p>
+      <StaticImage
+        alt="randomized unsplash image!"
+        src="https://source.unsplash.com/random/800x600"
+      />
+      </Layout>
+    </main>
+  )
+}
+
+export default IndexPage
+*/
+
+import * as React from 'react'
+import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
+const IndexPage = () => {
+  return (
+    <main>
+      <Layout pageTitle="Welcome to Inghelbrecht Agency!">
+      <p>Lorem ipsum</p>
+      <StaticImage
+        alt="Een Gatsby astronaut"
+        src="../images/linux-magic.jpg"
+      />
       </Layout>
     </main>
   )
